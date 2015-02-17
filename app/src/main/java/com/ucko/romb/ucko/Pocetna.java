314f4b31926638bf0b3afa48dde270881dd4129a
Lucky392 <lazar_92@live.com>
2015-02-17 15:19:04 +0100

    second

--- app/src/main/java/com/ucko/romb/ucko/Pocetna.java
+++ app/src/main/java/com/ucko/romb/ucko/Pocetna.java
@@ -1,39 +1,97 @@
 package com.ucko.romb.ucko;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View.*;
+import android.view.View;
 
 
 public class Pocetna extends ActionBarActivity {
+
+    Button radiLekcije;
+    Button upravljanjeSadrzajem;
+    Button opcije;
+    Button izlaz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pocetna);
-    }
+
+        radiLekcije = (Button) findViewById(R.id.btnRadiLekcije);
+        upravljanjeSadrzajem = (Button) findViewById(R.id.btnUpravljanjeSadrzajem);
+        opcije = (Button) findViewById(R.id.btnOpcije);
+        izlaz = (Button) findViewById(R.id.btnIzlaz);
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_pocetna, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        DatabaseHandler db = new DatabaseHandler(this);
+        String [] s = db.vratiPodesavanja();
+        if(s!= null && s[4].equals("c")){
+            setTitle(R.string.c_app_name);
+            radiLekcije.setText(R.string.c_radi_lekcije);
+            upravljanjeSadrzajem.setText(R.string.c_upravljanje_sadrzajem);
+            opcije.setText(R.string.c_opcije);
+            izlaz.setText(R.string.c_izlaz);
         }
 
-        return super.onOptionsItemSelected(item);
+        radiLekcije.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Pocetna.this, null);
+                i.putExtra("koji", 1);
+                i.putExtra("radjenje", true);
+                startActivity(i);
+            }
+        });
+
+        upravljanjeSadrzajem.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(Pocetna.this)
+                        .setMessage("Šta želite da uradite sa sadržajem?")
+                        .setCancelable(true)
+                        .setPositiveButton("Uredjivanje",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,
+                                                        int which) {
+                                        Intent i = new Intent(Pocetna.this, null);
+                                        startActivity(i);
+                                    }
+                                })
+                        .setNeutralButton("Napravi novo",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,
+                                                        int which) {
+
+                                        Intent i = new Intent(Pocetna.this, null);
+                                        startActivity(i);
+                                    }
+                                })
+                        .show();
+
+            }
+        });
+
+        opcije.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Pocetna.this, null));
+            }
+        });
+
+        izlaz.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
