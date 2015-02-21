fb46136a4f82f5bc4081cd106640e3a54642127e
Lucky392 <lazar_92@live.com>
2015-02-21 17:15:56 +0100

    -

--- app/src/main/java/com/ucko/romb/ucko/Pocetna.java
+++ app/src/main/java/com/ucko/romb/ucko/Pocetna.java
@@ -30,18 +30,6 @@ public class Pocetna extends ActionBarActivity {
         opcije = (Button) findViewById(R.id.btnOpcije);
         izlaz = (Button) findViewById(R.id.btnIzlaz);
 
-
-        DatabaseHandler db = new DatabaseHandler(this);
-        String[] s = db.vratiPodesavanja();
-        if (s != null && s[4].equals("c")) {
-            setTitle(R.string.c_app_name);
-            radiLekcije.setText(R.string.c_radi_lekcije);
-            novaLekcija.setText(R.string.c_upravljanje_sadrzajem);
-            urediPostojece.setText(R.string.c_uredi_postojece);
-            opcije.setText(R.string.c_opcije);
-            izlaz.setText(R.string.c_izlaz);
-        }
-
         radiLekcije.setOnClickListener(new OnClickListener() {
 
             @Override
@@ -50,6 +38,44 @@ public class Pocetna extends ActionBarActivity {
                 i.putExtra("radjenje", true);
                 i.putExtra("tabela", DatabaseHandler.LEKCIJE);
                 startActivity(i);
+            }
+        });
+
+        novaLekcija.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Pocetna.this, Lekcija.class);
+                i.putExtra("tabela", "nista");
+                startActivity(i);
+            }
+        });
+
+        urediPostojece.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Pocetna.this, Lekcija.class);
+                i.putExtra("radjenje", false);
+                i.putExtra("tabela", DatabaseHandler.LEKCIJE);
+                startActivity(i);
+            }
+        });
+
+        opcije.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Pocetna.this, Podesavanja.class);
+                startActivity(i);
+            }
+        });
+
+        izlaz.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
